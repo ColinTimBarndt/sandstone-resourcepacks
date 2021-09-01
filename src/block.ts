aad06dc874fece05a9750b01e53a4f029274e26e
ColinTimBarndt <colin.barndt@pm.me>
2021-09-01 15:45:00 +0200

    Literally typescript magic for Vector & BlockFace

--- src/block.ts
+++ src/block.ts
@@ -1,15 +1,25 @@
-import { VectorClass } from "sandstone";
+import { Vector, ArrayOfLength, EnumerateInt } from "./math";
 
 export type BlockFaceName = "north" | "east" | "south" | "west" | "up" | "down";
 export type BlockAxisName = "x" | "y" | "z";
 
-export class BlockFace {
-	public static readonly North = new BlockFace("north", 0, [0, 0, -1], "South");
-	public static readonly East = new BlockFace("east", 1, [1, 0, 0], "West");
-	public static readonly South = new BlockFace("south", 2, [0, 0, 1], "North");
-	public static readonly West = new BlockFace("west", 3, [-1, 0, 0], "East");
-	public static readonly Up = new BlockFace("up", 4, [0, 1, 0], "Down");
-	public static readonly Down = new BlockFace("down", 5, [0, -1, 0], "Up");
+type Unconst<A extends readonly unknown[], _S extends unknown[] = []> =
+	A["length"] extends _S["length"]
+	? _S
+	: Unconst<A, [..._S, A[_S["length"]]]>;
+
+export class BlockFace<
+	Name extends BlockFaceName = BlockFaceName,
+	Idx extends EnumerateInt<6> = EnumerateInt<6>,
+	Normal extends Readonly<ArrayOfLength<3, -1 | 0 | 1>> = Readonly<ArrayOfLength<3, -1 | 0 | 1>>,
+	OppositeName extends "North" | "East" | "South" | "West" | "Up" | "Down" = "North" | "East" | "South" | "West" | "Up" | "Down"
+	> {
+	public static readonly North = new BlockFace("north" as const, 0, [0, 0, -1] as const, "South" as const);
+	public static readonly East = new BlockFace("east" as const, 1, [1, 0, 0] as const, "West" as const);
+	public static readonly South = new BlockFace("south" as const, 2, [0, 0, 1] as const, "North" as const);
+	public static readonly West = new BlockFace("west" as const, 3, [-1, 0, 0] as const, "East" as const);
+	public static readonly Up = new BlockFace("up" as const, 4, [0, 1, 0] as const, "Down" as const);
+	public static readonly Down = new BlockFace("down" as const, 5, [0, -1, 0] as const, "Up" as const);
 
 	public static fromName(name: BlockFaceName): BlockFace;
 	public static fromName(name: string): BlockFace | null;
@@ -34,30 +44,30 @@ export class BlockFace {
 		BLOCK_FACES.forEach((face, id) => callback(face, id));
 	}
 
-	public readonly opposite!: BlockFace;
-	public readonly normal: VectorClass<[number, number, number]>;
-	public readonly isSide: boolean;
-	public readonly axis: BlockAxisName;
+	public readonly opposite!: (typeof BlockFace)[OppositeName];
+	public readonly normal: Vector<Unconst<Normal>>;
+	public readonly isSide: Name extends "north" | "east" | "south" | "west" ? true : false;
+	public readonly axis: Name extends "east" | "west" ? "x" : Name extends "up" | "down" ? "y" : "z";
 
 	private constructor(
-		public readonly name: BlockFaceName,
-		public readonly id: 0 | 1 | 2 | 3 | 4 | 5,
-		normal: [number, number, number],
-		opposite: keyof typeof BlockFace,
+		public readonly name: Name,
+		public readonly id: Idx,
+		normal: Normal,
+		opposite: OppositeName,
 	) {
-		this.normal = new VectorClass(normal);
-		if (normal[0] !== 0) this.axis = "x";
-		else if (normal[1] !== 0) this.axis = "y";
-		else this.axis = "z";
-		this.isSide = this.axis !== "y";
-		// temporary
+		this.normal = new Vector(normal as Unconst<typeof normal>);
+		if (normal[0] !== 0) this.axis = "x" as any;
+		else if (normal[1] !== 0) this.axis = "y" as any;
+		else this.axis = "z" as any;
+		this.isSide = (this.axis !== "y") as any;
+		// is set correctly later
 		(this as any).opposite = opposite;
 	}
 
-	public toString(): string {
+	public toString(): Name {
 		return this.name;
 	}
-	public toJSON(): string {
+	public toJSON(): Name {
 		return this.name;
 	}
 }
---
+++ src/math.ts
@@ -0,0 +1,226 @@
+import assert = require("node:assert");
+import { VectorClass } from "sandstone";
+
+/**
+ * All possible values of an index for a specific array as a type.
+ * @example
+ * ```ts
+ * const array = [2, 3, 6, 9, "f"] as const;
+ * type Index = 0 | 1 | 2 | 3 | 4;
+ * type Index = IndexOf<typeof array>;
+ * ```
+ */
+export type IndexOf<A extends unknown[] | readonly unknown[]> = _IndexOf<A>;
+type _IndexOf<A extends unknown[] | readonly unknown[], _S extends number[] = []> =
+	_S["length"] extends A["length"]
+	? _S[number]
+	: _IndexOf<A, [_S["length"], ..._S]>;
+/**
+ * An array of type T with length L.
+ * This is a short way of writing homogenous array types with a fixed length
+ * and is at the same time more readable.
+ * ```ts
+ * type Vec4 = [number, number, number, number];
+ * type Vec4 = ArrayOfLength<4, number>;
+ * ```
+ */
+export type ArrayOfLength<L extends number, T> = _ArrayOfLength<L, T>;
+type _ArrayOfLength<L extends number, T, _S extends T[] = []> =
+	_S["length"] extends L
+	? _S
+	: _ArrayOfLength<L, T, [..._S, T]>;
+
+/**
+ * Enumerates all numbers from 0 to exlusively A as a type.
+ * @example
+ * ```ts
+ * type Digits = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+ * type Digits = EnumerateInt<10>;
+ * ```
+ */
+export type EnumerateInt<A extends number> = _EnumerateInt<A>;
+type _EnumerateInt<A extends number, _S extends number[] = []> =
+	A extends _S["length"] ? _S[number] : _EnumerateInt<A, [..._S, _S["length"]]>;
+
+/**
+ * Enumerates all numbers from A to exclusively B as a type.
+ * @example
+ * ```
+ * type Nums = 2 | 3 | 4 | 5;
+ * type Nums = EnumerateRange<2, 6>;
+ * ```
+ */
+export type EnumerateRange<A extends number, B extends number> = _EnumerateRange<B, ArrayOfLength<A, 0>>;
+type _EnumerateRange<A extends number, B extends number[], C extends number[] = []> =
+	A extends B["length"]
+	? C[number]
+	: _EnumerateRange<A, [...B, 0], [...C, B["length"]]>;
+
+/**
+ * Get the length of an array as a type.
+ */
+export type Length<A extends unknown[] | readonly unknown[]> = A["length"];
+
+export type Vector4 = Vector<ArrayOfLength<4, number>>;
+export type Vector3 = Vector<ArrayOfLength<3, number>>;
+export type Vector2 = Vector<ArrayOfLength<2, number>>;
+export class Vector<T extends number[]> {
+	public readonly dimensions: T["length"];
+	private readonly _values: Readonly<T>;
+	constructor(
+		values: T,
+	) {
+		this._values = values;
+		this.dimensions = values.length;
+	}
+
+	/**
+	 * Gets a component of this vector
+	 */
+	public get<I extends IndexOf<T>>(idx: I): T[I];
+	public get(idx: number): T[number] | undefined;
+	public get(idx: number): T[number] | undefined {
+		return this._values[idx];
+	}
+	public get components(): T {
+		return this._values.slice() as T;
+	}
+
+	public get lengthSquared() {
+		return this._values.reduce((a, b) => a + b * b, 0);
+	}
+
+	public get length() {
+		return Math.sqrt(this.lengthSquared);
+	}
+
+	public [Symbol.iterator]() {
+		return this._values[Symbol.iterator]();
+	}
+
+	/**
+	 * Dot product
+	 */
+	public dot(...args: T | [Vector<T>]): number {
+		if (args.length === 1) {
+			const other = args[0] as Vector<T>;
+			assert(other.dimensions === this.dimensions, "Vectors do not have a matching length");
+			return this._values.map((n, i) => n * other._values[i as IndexOf<T>]).reduce((a, b) => a + b);
+		} else {
+			assert(args.length === this.dimensions, "Vectors do not have a matching length");
+			return this._values.map((n, i) => n * (args as T)[i as IndexOf<T>]).reduce((a, b) => a + b);
+		}
+	}
+
+	/**
+	 * Applies a map function to each component
+	 */
+	public map(mapFn: (n: T[IndexOf<T>], i: IndexOf<T>) => number): Vector<T> {
+		return new Vector(
+			this._values.map(mapFn as (n: number, i: number) => number)
+		) as Vector<T>;
+	}
+
+	public round(): Vector<T> {
+		return this.map(Math.round);
+	}
+	public floor(): Vector<T> {
+		return this.map(Math.floor);
+	}
+	public ceil(): Vector<T> {
+		return this.map(Math.ceil);
+	}
+	public square(): Vector<T> {
+		return this.map(n => n * n);
+	}
+
+	public add(vector: Vector<T>): Vector<T>;
+	public add(...vector: T): Vector<T>;
+	public add(all: number): Vector<T>;
+	public add(...args: T | [Vector<T> | number]): Vector<T> {
+		if (args.length === 1) {
+			const other = args[0];
+			if (other instanceof Vector) {
+				assert(other.dimensions === this.dimensions, "Vectors do not have a matching length");
+				return new Vector(this._values.map((n, i) => n + other._values[i as IndexOf<T>])) as Vector<T>;
+			} else {
+				return new Vector(this._values.map(n => n + other)) as Vector<T>;
+			}
+		} else {
+			assert(args.length === this.dimensions, "Vectors do not have a matching length");
+			return new Vector(this._values.map((n, i) => n + (args as T)[i as IndexOf<T>])) as Vector<T>;
+		}
+	}
+
+	public sub(vector: Vector<T>): Vector<T>;
+	public sub(...vector: T): Vector<T>;
+	public sub(all: number): Vector<T>;
+	public sub(...args: T | [Vector<T> | number]): Vector<T> {
+		if (args.length === 1) {
+			const other = args[0];
+			if (other instanceof Vector) {
+				assert(other.dimensions === this.dimensions, "Vectors do not have a matching length");
+				return new Vector(this._values.map((n, i) => n - other._values[i as IndexOf<T>])) as Vector<T>;
+			} else {
+				return new Vector(this._values.map(n => n - other)) as Vector<T>;
+			}
+		} else {
+			assert(args.length === this.dimensions, "Vectors do not have a matching length");
+			return new Vector(this._values.map((n, i) => n - (args as T)[i as IndexOf<T>])) as Vector<T>;
+		}
+	}
+
+	public mul(vector: Vector<T>): Vector<T>;
+	public mul(...vector: T): Vector<T>;
+	public mul(all: number): Vector<T>;
+	public mul(...args: T | [Vector<T> | number]): Vector<T> {
+		if (args.length === 1) {
+			const other = args[0];
+			if (other instanceof Vector) {
+				assert(other.dimensions === this.dimensions, "Vectors do not have a matching length");
+				return new Vector(this._values.map((n, i) => n * other._values[i as IndexOf<T>])) as Vector<T>;
+			} else {
+				return new Vector(this._values.map(n => n * other)) as Vector<T>;
+			}
+		} else {
+			assert(args.length === this.dimensions, "Vectors do not have a matching length");
+			return new Vector(this._values.map((n, i) => n * (args as T)[i as IndexOf<T>])) as Vector<T>;
+		}
+	}
+
+	public div(vector: Vector<T>): Vector<T>;
+	public div(...vector: T): Vector<T>;
+	public div(all: number): Vector<T>;
+	public div(...args: T | [Vector<T> | number]): Vector<T> {
+		if (args.length === 1) {
+			const other = args[0];
+			if (other instanceof Vector) {
+				assert(other.dimensions === this.dimensions, "Vectors do not have a matching length");
+				return new Vector(this._values.map((n, i) => n / other._values[i as IndexOf<T>])) as Vector<T>;
+			} else {
+				return new Vector(this._values.map(n => n / other)) as Vector<T>;
+			}
+		} else {
+			assert(args.length === this.dimensions, "Vectors do not have a matching length");
+			return new Vector(this._values.map((n, i) => n / (args as T)[i as IndexOf<T>])) as Vector<T>;
+		}
+	}
+
+	public toCoordsAbs(): VectorClass<ArrayOfLength<Length<T>, `${number}`>> {
+		return new VectorClass(
+			this._values.map(n => `${n}` as const) as ArrayOfLength<Length<T>, `${number}`>
+		);
+	}
+
+	public toCoordsRel(): VectorClass<ArrayOfLength<Length<T>, `~${number}`>> {
+		return new VectorClass(
+			this._values.map(n => `~${n}` as const) as ArrayOfLength<Length<T>, `~${number}`>
+		);
+	}
+
+	public toCoordsLoc(): VectorClass<ArrayOfLength<Length<T>, `^${number}`>> {
+		return new VectorClass(
+			this._values.map(n => `^${n}` as const) as ArrayOfLength<Length<T>, `^${number}`>
+		);
+	}
+}
